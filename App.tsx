230cddba154f36559499ad6758728a74fd9770c2
mandorakannu <mandorakannu@gmail.com>
2023-09-25 14:54:44 +0530

    GIT INIT

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+function App(): JSX.Element {
+  return <></>;
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
